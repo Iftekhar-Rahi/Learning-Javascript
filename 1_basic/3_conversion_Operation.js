03b3b1d27a15fe28c0ebd7f2d7b2f9521ca7ca6d
Iftekhar-Rahi <iftekharhossainrahi@gmail.com>
2025-11-11 23:26:55 +0600

    3_conversion_Operation

--- 1_basic/3_conversion_Operation.js
+++ 1_basic/3_conversion_Operation.js
@@ -1,3 +1,7 @@
-let score=33
+let score="33a"
 console.log("score is "+score) // score is 33
 console.log(typeof score) // number
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber) // number
+console.log(valueInNumber) // number
+console.log("score is "+valueInNumber)
