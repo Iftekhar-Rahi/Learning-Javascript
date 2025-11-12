7aaf161aa80c67676e09cf2a12496141a2435d25
Iftekhar-Rahi <iftekharhossainrahi@gmail.com>
2025-11-12 21:42:53 +0600

    3.5 Conversion

---
+++ 1_basic/3_5_Operation.js
@@ -0,0 +1,23 @@
+let value=3
+let negValue=-value
+// console.log(value)
+// console.log(negValue)
+
+// 
+// console.log()==print()
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+// 
+
+let str1="hello"
+let str2=" Rahi"
+let str3=str1+str2
+// console.log(str3)
+ console.log("2"+2) // "22"
+ console.log(2+"2") // "22"
+ console.log("2"+ "2") // "22"
+ console.log("hello"+ 34) // "hello34"
