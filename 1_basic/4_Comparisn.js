70e5d74b4204cd981de0bf424ff3a919eb1aaae8
Iftekhar-Rahi <iftekharhossainrahi@gmail.com>
2025-11-13 23:47:34 +0600

    Comparisn

--- 1_basic/4_Comparisn.js
+++ 1_basic/4_Comparisn.js
@@ -8,13 +8,17 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+
+
+// Should have to avoid the below 6 cases
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
 // === 
 
